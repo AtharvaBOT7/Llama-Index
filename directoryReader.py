d54fd1bc87759b32b02c38d7023b2ef72f31266f
AtharvaBOT7 <atharvachundurwar841@gmail.com>
2025-06-03 12:15:39 -0700

    data folder added for directory loader, working directory loader code added

--- directoryReader.py
+++ directoryReader.py
@@ -1,4 +1,5 @@
-from llama_index.readers.file.base import SimpleDirectoryReader
+from urllib import response
+from llama_index.core import SimpleDirectoryReader
 from llama_index.core import VectorStoreIndex
 import os
 from dotenv import load_dotenv
@@ -7,3 +8,12 @@ import sys
 
 load_dotenv()
 
+def main(url: str) -> None:
+    documents = SimpleDirectoryReader(url).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What is this research paper trying to convey, please summarise.")
+    print(response)
+
+if __name__ == "__main__":
+    main(url="/Users/atharva7/Downloads/Llama Index/data")
